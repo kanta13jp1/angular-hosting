632901b728426b2fda7ea233bce281db6971fd2e
kanta13jp1 <kanta13jp@gmail.com>
2021-01-11 19:37:39 +0900

    ng generate component heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,20 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: '玉木雄一郎' },
+  { id: 11, name: '山尾志桜里' },
+  { id: 12, name: '伊藤' },
+  { id: 13, name: '矢田' },
+  { id: 14, name: '岸本' },
+  { id: 15, name: '西岡' },
+  { id: 16, name: '浅野哲' },
+  { id: 17, name: '榛葉' },
+  { id: 18, name: '古川' },
+  { id: 19, name: '足立' },
+  { id: 20, name: '小林' },
+  { id: 21, name: '浜野' },
+  { id: 22, name: '舟山' },
+  { id: 23, name: '川合' },
+  { id: 24, name: '大塚' },
+  { id: 25, name: '前原' }
+];
