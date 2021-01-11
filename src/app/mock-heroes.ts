d7f937ed3343e4622569b43000395f90d5d156a8
kanta13jp1 <kanta13jp@gmail.com>
2021-01-11 20:00:08 +0900

    ng generate component hero-detail

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,20 +1,20 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 1, name: '玉木雄一郎' },
-  { id: 11, name: '山尾志桜里' },
-  { id: 12, name: '伊藤' },
-  { id: 13, name: '矢田' },
-  { id: 14, name: '岸本' },
-  { id: 15, name: '西岡' },
-  { id: 16, name: '浅野哲' },
-  { id: 17, name: '榛葉' },
-  { id: 18, name: '古川' },
-  { id: 19, name: '足立' },
-  { id: 20, name: '小林' },
-  { id: 21, name: '浜野' },
-  { id: 22, name: '舟山' },
-  { id: 23, name: '川合' },
-  { id: 24, name: '大塚' },
-  { id: 25, name: '前原' }
+  { id: 1, name: '玉木雄一郎', role: '代表'},
+  { id: 11, name: '山尾志桜里', role: '' },
+  { id: 12, name: '伊藤孝恵', role: '副代表' },
+  { id: 13, name: '矢田わか子', role: '副代表' },
+  { id: 14, name: '岸本', role: '' },
+  { id: 15, name: '西岡', role: '' },
+  { id: 16, name: '浅野哲', role: '' },
+  { id: 17, name: '榛葉', role: '' },
+  { id: 18, name: '古川', role: '' },
+  { id: 19, name: '足立', role: '' },
+  { id: 20, name: '小林', role: '' },
+  { id: 21, name: '浜野', role: '' },
+  { id: 22, name: '舟山', role: '' },
+  { id: 23, name: '川合', role: '' },
+  { id: 24, name: '大塚', role: '' },
+  { id: 25, name: '前原', role: '' }
 ];
