b16826f8a3ea37021810841cc30ff10dedea67db
kanta13jp1 <kanta13jp@gmail.com>
2021-01-16 21:07:39 +0900

    議員データ更新

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -4,4 +4,5 @@ export interface Hero {
   role: string;
   belongs: string;
   almaMater: string;
+  district: string;
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,20 +1,20 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 1, name: '玉木雄一郎', role: '代表', belongs: '衆議院', almaMater: ''},
-  { id: 2, name: '山尾志桜里', role: '広報局長', belongs: '衆議院', almaMater: '' },
-  { id: 3, name: '伊藤孝恵', role: '副代表', belongs: '参議院', almaMater: '' },
-  { id: 4, name: '矢田わか子', role: '副代表', belongs: '参議院', almaMater: '' },
-  { id: 5, name: '岸本周平', role: '幹事長代理', belongs: '衆議院', almaMater: '' },
-  { id: 6, name: '西岡秀子', role: '政務調査会長代理', belongs: '衆議院', almaMater: '' },
-  { id: 7, name: '浅野哲', role: '国対委員長代理', belongs: '衆議院', almaMater: '' },
-  { id: 8, name: '榛葉賀津也', role: '幹事長', belongs: '衆議院', almaMater: '' },
-  { id: 9, name: '古川元久', role: '国対委員長', belongs: '衆議院', almaMater: '' },
-  { id: 10, name: '足立信也', role: '組織・団体委員長', belongs: '衆議院', almaMater: '' },
-  { id: 11, name: '小林正夫', role: '倫理委員長', belongs: '参議院', almaMater: '' },
-  { id: 12, name: '浜野善史', role: '財務局長', belongs: '参議院', almaMater: '神戸大学経済学部' },
-  { id: 13, name: '舟山康江', role: '政務調査会長', belongs: '衆議院', almaMater: '' },
-  { id: 14, name: '川合孝典', role: '参議院国会対策委員長', belongs: '参議院', almaMater: '' },
-  { id: 15, name: '大塚耕平', role: '代表代行', belongs: '衆議院', almaMater: '' },
-  { id: 16, name: '前原誠司', role: '代表代行', belongs: '衆議院', almaMater: '' }
+  { id: 1, name: '玉木雄一郎', role: '代表', belongs: '衆議院', almaMater: '東京大学法学部', district: '香川2区'},
+  { id: 2, name: '山尾志桜里', role: '広報局長', belongs: '衆議院', almaMater: '', district: '' },
+  { id: 3, name: '伊藤孝恵', role: '副代表', belongs: '参議院', almaMater: '', district: '' },
+  { id: 4, name: '矢田わか子', role: '副代表', belongs: '参議院', almaMater: '', district: '' },
+  { id: 5, name: '岸本周平', role: '幹事長代理', belongs: '衆議院', almaMater: '', district: '' },
+  { id: 6, name: '西岡秀子', role: '政務調査会長代理', belongs: '衆議院', almaMater: '', district: '' },
+  { id: 7, name: '浅野哲', role: '国対委員長代理', belongs: '衆議院', almaMater: '', district: '' },
+  { id: 8, name: '榛葉賀津也', role: '幹事長', belongs: '衆議院', almaMater: '', district: '' },
+  { id: 9, name: '古川元久', role: '国対委員長', belongs: '衆議院', almaMater: '', district: '' },
+  { id: 10, name: '足立信也', role: '組織・団体委員長', belongs: '衆議院', almaMater: '', district: '' },
+  { id: 11, name: '小林正夫', role: '倫理委員長', belongs: '参議院', almaMater: '', district: '' },
+  { id: 12, name: '浜野善史', role: '財務局長', belongs: '参議院', almaMater: '神戸大学経済学部', district: '' },
+  { id: 13, name: '舟山康江', role: '政務調査会長', belongs: '衆議院', almaMater: '', district: '' },
+  { id: 14, name: '川合孝典', role: '参議院国会対策委員長', belongs: '参議院', almaMater: '', district: ''},
+  { id: 15, name: '大塚耕平', role: '代表代行', belongs: '衆議院', almaMater: '', district: '' },
+  { id: 16, name: '前原誠司', role: '代表代行', belongs: '衆議院', almaMater: '', district: '' }
 ];
