7aacf6601d29a94fd666a36ba296927760510d50
kanta13jp1 <kanta13jp@gmail.com>
2021-01-16 17:28:01 +0900

    画面デザイン更新

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -3,8 +3,8 @@ import { Hero } from './hero';
 export const HEROES: Hero[] = [
   { id: 1, name: '玉木雄一郎', role: '代表', belongs: '衆議院'},
   { id: 2, name: '山尾志桜里', role: '広報局長', belongs: '衆議院' },
-  { id: 12, name: '伊藤孝恵', role: '副代表', belongs: '参議院' },
-  { id: 13, name: '矢田わか子', role: '副代表', belongs: '参議院' },
+  { id: 3, name: '伊藤孝恵', role: '副代表', belongs: '参議院' },
+  { id: 4, name: '矢田わか子', role: '副代表', belongs: '参議院' },
   { id: 14, name: '岸本周平', role: '幹事長代理', belongs: '衆議院' },
   { id: 15, name: '西岡秀子', role: '政務調査会長代理', belongs: '衆議院' },
   { id: 16, name: '浅野哲', role: '国対委員長代理', belongs: '衆議院' },
@@ -13,7 +13,7 @@ export const HEROES: Hero[] = [
   { id: 19, name: '足立', role: '', belongs: '衆議院' },
   { id: 20, name: '小林', role: '', belongs: '衆議院' },
   { id: 21, name: '浜野', role: '', belongs: '衆議院' },
-  { id: 22, name: '舟山', role: '', belongs: '衆議院' },
+  { id: 22, name: '舟山康江', role: '政務調査会長', belongs: '衆議院' },
   { id: 23, name: '川合', role: '', belongs: '衆議院' },
   { id: 24, name: '大塚', role: '', belongs: '衆議院' },
   { id: 25, name: '前原', role: '', belongs: '衆議院' }
