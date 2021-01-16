30f2e8923b3dcd54dfd3dafa99c4d6928c5988b6
kanta13jp1 <kanta13jp@gmail.com>
2021-01-16 19:59:19 +0900

    保存機能実装

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -13,17 +13,17 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 3, name: '伊藤孝恵', role: '副代表', belongs: '参議院' },
       { id: 4, name: '矢田わか子', role: '副代表', belongs: '参議院' },
       { id: 5, name: '岸本周平', role: '幹事長代理', belongs: '衆議院' },
-      { id: 15, name: '西岡秀子', role: '政務調査会長代理', belongs: '衆議院' },
-      { id: 16, name: '浅野哲', role: '国対委員長代理', belongs: '衆議院' },
-      { id: 17, name: '榛葉賀津也', role: '幹事長', belongs: '衆議院' },
+      { id: 6, name: '西岡秀子', role: '政務調査会長代理', belongs: '衆議院' },
+      { id: 7, name: '浅野哲', role: '国対委員長代理', belongs: '衆議院' },
+      { id: 8, name: '榛葉賀津也', role: '幹事長', belongs: '衆議院' },
       { id: 18, name: '古川元久', role: '国対委員長', belongs: '衆議院' },
       { id: 19, name: '足立信也', role: '組織・団体委員長', belongs: '衆議院' },
-      { id: 20, name: '小林', role: '', belongs: '衆議院' },
+      { id: 20, name: '小林正夫', role: '倫理委員長', belongs: '参議院' },
       { id: 21, name: '浜野', role: '', belongs: '衆議院' },
       { id: 22, name: '舟山康江', role: '政務調査会長', belongs: '衆議院' },
       { id: 23, name: '川合', role: '', belongs: '衆議院' },
       { id: 24, name: '大塚', role: '', belongs: '衆議院' },
-      { id: 25, name: '前原', role: '', belongs: '衆議院' }
+      { id: 25, name: '前原誠司', role: '代表代行', belongs: '衆議院' }
     ];
     return {heroes};
   }
