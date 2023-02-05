b89a17a4ad6982f7107119ce30d038ba23cc4637
kanta13jp1 <kanta13jp@gmail.com>
2023-02-06 07:22:27 +0900

    フィルター機能修正

--- src/app/local-candidate/local-candidate.component.ts
+++ src/app/local-candidate/local-candidate.component.ts
@@ -9,7 +9,11 @@ import { DatePipe } from '@angular/common';
 })
 export class LocalCandidateComponent implements OnInit {
   heroes!: Hero[];
+  shins!: Hero[];
+  gens!: Hero[];
   recommends!: Hero[];
+  recomgens!: Hero[];
+  recomshins!: Hero[];
   block: string;
   dispblock: string;
   nowDate: Date;
@@ -66,113 +70,225 @@ export class LocalCandidateComponent implements OnInit {
 
   getLocalCandidates(): void {
     if (this.checkboxes.genshoku && this.checkboxes.shinjin && this.checkboxes.suigen && this.checkboxes.suishin) {
-      this.heroService.getLocalCandidates()
-        .subscribe(heroes => this.heroes = heroes.slice(0, 100));
-        this.filterRecommend();
+      console.log("現職 + 新人 + 推薦現職 + 推薦新人")
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','現職')
+        .subscribe(gens => {
+          this.gens = gens.slice(0, 100)
+        })
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','新人')
+        .subscribe(shins => {
+          this.shins = shins.slice(0, 100)
+        })
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','現職')
+        .subscribe(recomgens => this.recomgens = recomgens.slice(0, 100))
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','新人')
+        .subscribe(recomshins => {
+          this.recomshins = recomshins.slice(0, 100)
+          this.recommends = this.recomgens.concat(this.recomshins)
+          this.heroes = this.gens.concat(this.shins)
+          this.heroes = this.heroes.concat(this.recommends).sort((a, b)=> a.id - b.id)
+        })
       } else if (this.checkboxes.genshoku && this.checkboxes.shinjin && this.checkboxes.suigen) {
         console.log("現職 + 新人 + 推薦現職")
-        this.heroService.searchLocalCandidatesByLisence('公認')
-            .subscribe(heroes => this.heroes = heroes.slice(0, 100))
-        this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','現職')
-            .subscribe(recommends => {
-              this.recommends = recommends.slice(0, 100)
-              this.heroes = this.heroes.concat(this.recommends)
-            })
+        this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','現職')
+        .subscribe(gens => {
+          this.gens = gens.slice(0, 100)
+        })
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','新人')
+        .subscribe(shins => {
+          this.shins = shins.slice(0, 100)
+        })
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','現職')
+        .subscribe(recomgens => {
+          this.recomgens = recomgens.slice(0, 100)
+          this.recomshins = []
+          this.recommends = this.recomgens.concat(this.recomshins)
+          this.heroes = this.gens.concat(this.shins)
+          this.heroes = this.heroes.concat(this.recommends).sort((a, b)=> a.id - b.id)
+        })
     } else if (this.checkboxes.genshoku && this.checkboxes.shinjin && this.checkboxes.suishin) {
-            console.log("現職 + 新人 + 推薦新人")
-            this.heroService.searchLocalCandidatesByLisence('公認')
-                .subscribe(heroes => this.heroes = heroes.slice(0, 100))
-            this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','新人')
-                .subscribe(recommends => {
-                  this.recommends = recommends.slice(0, 100)
-                  this.heroes = this.heroes.concat(this.recommends)
-                })
-
+      console.log("現職 + 新人 + 推薦新人")
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','現職')
+        .subscribe(gens => {
+          this.gens = gens.slice(0, 100)
+        })
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','新人')
+        .subscribe(shins => {
+          this.shins = shins.slice(0, 100)
+        })
+    this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','新人')
+        .subscribe(recomshins => {
+          this.recomgens = []
+          this.recomshins = recomshins.slice(0, 100)
+          this.recommends = this.recomgens.concat(this.recomshins)
+          this.heroes = this.gens.concat(this.shins)
+          this.heroes = this.heroes.concat(this.recommends).sort((a, b)=> a.id - b.id)
+      })
     } else if (this.checkboxes.genshoku && this.checkboxes.shinjin ) {
         console.log("現職 + 新人")
-        this.heroService.searchLocalCandidatesByLisence('公認')
-          .subscribe(heroes => this.heroes = heroes.slice(0, 100));
-        this.recommends = [];
-      } else if (this.checkboxes.shinjin && this.checkboxes.suigen && this.checkboxes.suishin) {
-        console.log("新人 + 推薦現職 + 推薦新人")
-        this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','新人')
-          .subscribe(heroes => this.heroes = heroes.slice(0, 100));
-        this.heroService.searchLocalCandidatesByLisence('推薦')
-          .subscribe(recommends => {
-            this.recommends = recommends.slice(0, 100)
-            this.heroes = this.heroes.concat(this.recommends)});
-          } else if (this.checkboxes.shinjin && this.checkboxes.suigen) {
-            console.log("新人 + 推薦現職")
-            this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','新人')
-                .subscribe(heroes => this.heroes = heroes.slice(0, 100))
-            this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','現職')
-                .subscribe(recommends => {
-                  this.recommends = recommends.slice(0, 100)
-                  this.heroes = this.heroes.concat(this.recommends)
-                })
-              } else if (this.checkboxes.shinjin && this.checkboxes.suishin) {
-                console.log("新人 + 推薦新人")
-                this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','新人')
-                    .subscribe(heroes => this.heroes = heroes.slice(0, 100))
-                this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','新人')
-                    .subscribe(recommends => {
-                      this.recommends = recommends.slice(0, 100)
-                      this.heroes = this.heroes.concat(this.recommends)
-                    })
-          } else if (this.checkboxes.shinjin) {
-            console.log("新人のみ")
-            this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','新人')
-      .subscribe(heroes => this.heroes = heroes.slice(0, 100));
-      this.recommends = [];
+        this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','現職')
+        .subscribe(gens => {
+          this.gens = gens.slice(0, 100)
+        })
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','新人')
+        .subscribe(shins => {
+          this.shins = shins.slice(0, 100)
+          this.heroes = this.gens.concat(this.shins)
+          this.heroes = this.heroes.concat(this.recommends).sort((a, b)=> a.id - b.id)
+          this.recommends = [];
+          this.recomgens = [];
+          this.recomshins = [];
+        })
+    } else if (this.checkboxes.shinjin && this.checkboxes.suigen && this.checkboxes.suishin) {
+      console.log("新人 + 推薦現職 + 推薦新人")
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','新人')
+        .subscribe(shins => {
+          this.shins = shins.slice(0, 100)
+          this.gens = []
+        });
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','現職')
+        .subscribe(recomgens => this.recomgens = recomgens.slice(0, 100))
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','新人')
+        .subscribe(recomshins => {
+          this.recomshins = recomshins.slice(0, 100)
+          this.recommends = this.recomgens.concat(this.recomshins)
+          this.heroes = this.shins.concat(this.recommends).sort((a, b)=> a.id - b.id)
+        })
+    } else if (this.checkboxes.shinjin && this.checkboxes.suigen) {
+      console.log("新人 + 推薦現職")
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','新人')
+        .subscribe(shins => {
+          this.shins = shins.slice(0, 100)
+          this.gens = []
+        });
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','現職')
+        .subscribe(recomgens => {
+          this.recomgens = recomgens.slice(0, 100)
+          this.recomshins = []
+          this.recommends = this.recomgens.concat(this.recomshins)
+          this.heroes = this.shins.concat(this.recommends).sort((a, b)=> a.id - b.id)
+        })
+    } else if (this.checkboxes.shinjin && this.checkboxes.suishin) {
+      console.log("新人 + 推薦新人")
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','新人')
+        .subscribe(shins => {
+          this.shins = shins.slice(0, 100)
+          this.gens = []
+        });
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','新人')
+      .subscribe(recomshins => {
+        this.recomgens = []
+        this.recomshins = recomshins.slice(0, 100)
+        this.recommends = this.recomgens.concat(this.recomshins)
+        this.heroes = this.shins.concat(this.recommends).sort((a, b)=> a.id - b.id)
+      })
+    } else if (this.checkboxes.shinjin) {
+      console.log("新人のみ")
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','新人')
+        .subscribe(shins => {
+          this.shins = shins.slice(0, 100)
+          this.gens = []
+          this.recomgens = []
+          this.recomshins =[]
+          this.recommends = this.recomgens.concat(this.recomshins)
+          this.heroes = this.shins.concat(this.recommends).sort((a, b)=> a.id - b.id)
+        });
     } else if (this.checkboxes.genshoku && this.checkboxes.suigen && this.checkboxes.suishin) {
       console.log("現職 + 推薦現職 + 推薦新人")
       this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','現職')
-        .subscribe(heroes => this.heroes = heroes.slice(0, 100));
-      this.heroService.searchLocalCandidatesByLisence('推薦')
-        .subscribe(recommends => {
-          this.recommends = recommends.slice(0, 100)
-          this.heroes = this.heroes.concat(this.recommends)});
+        .subscribe(gens => {
+          this.gens = gens.slice(0, 100)
+          this.shins = []
+        });
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','現職')
+        .subscribe(recomgens => this.recomgens = recomgens.slice(0, 100))
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','新人')
+        .subscribe(recomshins => {
+          this.recomshins = recomshins.slice(0, 100)
+          this.recommends = this.recomgens.concat(this.recomshins)
+          this.heroes = this.gens.concat(this.recommends).sort((a, b)=> a.id - b.id)
+        })
     } else if (this.checkboxes.suigen && this.checkboxes.suishin) {
       console.log("推薦現職 + 推薦新人")
-      this.heroService.searchLocalCandidatesByLisence('推薦')
-        .subscribe(heroes => {
-          this.heroes = heroes.slice(0, 100)
-          this.recommends = this.heroes});
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','現職')
+        .subscribe(recomgens => this.recomgens = recomgens.slice(0, 100))
+      this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','新人')
+        .subscribe(recomshins => {
+          this.gens = []
+          this.shins = []
+          this.recomshins = recomshins.slice(0, 100)
+          this.recommends = this.recomgens.concat(this.recomshins)
+          this.heroes = this.recommends.sort((a, b)=> a.id - b.id)
+        })
     } else if (this.checkboxes.genshoku && this.checkboxes.suigen) {
       console.log("現職 + 推薦現職")
       this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','現職')
-          .subscribe(heroes => this.heroes = heroes.slice(0, 100))
+        .subscribe(gens => {
+          this.gens = gens.slice(0, 100)
+          this.shins = []
+        });
       this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','現職')
-          .subscribe(recommends => {
-            this.recommends = recommends.slice(0, 100)
-            this.heroes = this.heroes.concat(this.recommends)
-          })
-        } else if (this.checkboxes.genshoku && this.checkboxes.suishin) {
-          console.log("現職 + 推薦新人")
+        .subscribe(recomgens => {
+          this.recomgens = recomgens.slice(0, 100)
+          this.recomshins = []
+          this.recommends = this.recomgens.concat(this.recomshins)
+          this.heroes = this.gens.concat(this.recommends).sort((a, b)=> a.id - b.id)
+        })
+  } else if (this.checkboxes.genshoku && this.checkboxes.suishin) {
+    console.log("現職 + 推薦新人")
+    this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','現職')
+      .subscribe(gens => {
+        this.gens = gens.slice(0, 100)
+        this.shins = []
+    });
+    this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','現職')
+      .subscribe(recomshins => {
+        this.recomshins = recomshins.slice(0, 100)
+        this.recomgens = []
+        this.recommends = this.recomgens.concat(this.recomshins)
+        this.heroes = this.gens.concat(this.recommends).sort((a, b)=> a.id - b.id)
+    })
+  } else if (this.checkboxes.genshoku) {
+          console.log("現職のみ")
           this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','現職')
-              .subscribe(heroes => this.heroes = heroes.slice(0, 100))
-          this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','新人')
-              .subscribe(recommends => {
-                this.recommends = recommends.slice(0, 100)
-                this.heroes = this.heroes.concat(this.recommends)
-              })
-        } else if (this.checkboxes.genshoku) {
-        console.log("現職のみ")
-        this.heroService.searchLocalCandidatesByLisenceAndNewcomer('公認','現職')
-          .subscribe(heroes => this.heroes = heroes.slice(0, 100));
-        this.recommends = [];
-    }else if (this.checkboxes.suigen) {
+          .subscribe(gens => {
+            this.gens = gens.slice(0, 100)
+            this.shins = []
+            this.recomgens = []
+            this.recomshins =[]
+            this.recommends = this.recomgens.concat(this.recomshins)
+            this.heroes = this.gens.concat(this.recommends).sort((a, b)=> a.id - b.id)
+          });
+  } else if (this.checkboxes.suigen) {
       console.log("推薦現職のみ")
       this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','現職')
-        .subscribe(heroes => {
-          this.heroes = heroes.slice(0, 100)
-          this.recommends = this.heroes});
+        .subscribe(recomgens => {
+          this.gens = []
+          this.shins = []
+          this.recomgens = recomgens.slice(0, 100)
+          this.recomshins = []
+          this.recommends = this.recomgens.concat(this.recomshins)
+          this.heroes = this.gens.concat(this.recommends).sort((a, b)=> a.id - b.id)
+        });
     } else if (this.checkboxes.suishin) {
       console.log("推薦新人のみ")
       this.heroService.searchLocalCandidatesByLisenceAndNewcomer('推薦','新人')
-      .subscribe(heroes => {
-        this.heroes = heroes.slice(0, 100)
-        this.recommends = this.heroes});
+        .subscribe(recomshins => {
+          this.gens = []
+          this.shins = []
+          this.recomshins = recomshins.slice(0, 100)
+          this.recomgens = []
+          this.recommends = this.recomgens.concat(this.recomshins)
+          this.heroes = this.gens.concat(this.recommends).sort((a, b)=> a.id - b.id)
+        });
+  } else {
+      console.log("なし")
+      this.heroes = []
+      this.shins = []
+      this.gens = []
+      this.recommends = this.heroes
+      this.recomshins = []
+      this.recomgens = []
     }
     this.block = '全候補者'
     this.dispblock = this.block;
